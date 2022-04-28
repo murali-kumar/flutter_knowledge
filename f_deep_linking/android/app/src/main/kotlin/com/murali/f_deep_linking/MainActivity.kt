97e4ccf8ac111f2d5bc72e99f4ceb160d41508a7
murali kumar <muraliofficialid@gmail.com>
2022-04-28 20:32:27 +0530

    init

---
+++ f_deep_linking/android/app/src/main/kotlin/com/murali/f_deep_linking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.murali.f_deep_linking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ f_method_channel/android/app/src/main/kotlin/com/murali/f_method_channel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.murali.f_method_channel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
